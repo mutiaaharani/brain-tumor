3819c2efdb186a941d32a1338ccbbe2049bf6b7d
mutiaaharani <12350321282@students.uin-suska.ac.id>
2026-06-20 18:50:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-
+from gradcam import (
+    make_gradcam_heatmap,
+    overlay_gradcam
+)
 import streamlit as st
 import tensorflow as tf
 import numpy as np
@@ -70,3 +73,36 @@ if uploaded_file:
     st.info(
         f"Confidence: {confidence:.2f}%"
     )
+gradcam_input = image.resize(
+    (224,224)
+)
+
+gradcam_input = np.array(
+    gradcam_input
+).astype(np.float32)
+
+gradcam_input = gradcam_input / 255.0
+
+gradcam_input = np.expand_dims(
+    gradcam_input,
+    axis=0
+)
+
+heatmap = make_gradcam_heatmap(
+    gradcam_input
+)
+
+overlay = overlay_gradcam(
+    np.array(image),
+    heatmap
+)
+
+st.subheader(
+    "Grad-CAM Visualization"
+)
+
+st.image(
+    overlay,
+    caption="Area yang menjadi fokus model",
+    use_container_width=True
+)
